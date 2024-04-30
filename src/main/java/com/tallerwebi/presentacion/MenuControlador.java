6038fcd6be2a80ad74ba4828b9d5b1e8fd2ebde0
AlanAruFlores <alanaruquipa242@gmail.com>
2024-04-30 19:47:31 -0300

    menu y perfil sincronizados

--- src/main/java/com/tallerwebi/presentacion/MenuControlador.java
+++ src/main/java/com/tallerwebi/presentacion/MenuControlador.java
@@ -6,22 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class MonopolyMenuControlador {
+public class MenuControlador {
 
     @RequestMapping("/ir-menu")
     public ModelAndView irAlMenu() {
         return new ModelAndView("menu.html");
     }
-
     @RequestMapping("/ir-jugar")
     public ModelAndView irAJugar(){
         return new ModelAndView("jugar.html");
     }
 
-    @RequestMapping("/ir-estadistica")
-    public ModelAndView irAEstadistica(){
-        return new ModelAndView("estadistica.html");
-    }
 
     @RequestMapping("/ir-salir")
     public ModelAndView irASalir(){
