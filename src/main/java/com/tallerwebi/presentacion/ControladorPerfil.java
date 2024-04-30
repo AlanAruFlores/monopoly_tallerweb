97edfb4652361b49b24afdb324f6281f52b2653b
Milagros Pessino <milipessi@gmail.com>
2024-04-30 15:39:49 -0300

    perfilParaEmpezar

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorPerfil.java
@@ -0,0 +1,20 @@
+package com.tallerwebi.presentacion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorPerfil {
+    @RequestMapping("/perfil")
+    public ModelAndView irAlperfil() {
+        return new ModelAndView("perfil.html");
+    }
+
+    @RequestMapping("/editar-perfil")
+    public ModelAndView editarPerfil() {
+        return new ModelAndView("editarPerfil.html");
+    }
+
+
+}
