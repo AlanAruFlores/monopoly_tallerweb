7e3b637831ce380a1c3d3a55e9a625dbeecac8c4
AlanAruFlores <alanaruquipa242@gmail.com>
2024-05-07 22:01:27 -0300

    Tablero implementado

--- src/main/java/com/tallerwebi/presentacion/ControladorMonopoly.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorMonopoly.java
@@ -1,17 +1,44 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.ServicioMonopoly;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ControladorMonopoly {
 
-    @RequestMapping("/monopoly")
-    public String showMonopolyPage() {
-        return "monopoly";
+    ServicioMonopoly servicioMonopoly;
+
+    @Autowired
+    public ControladorMonopoly(ServicioMonopoly servicioMonopoly){
+        this.servicioMonopoly = servicioMonopoly;
     }
 
+    @RequestMapping("/monopoly")
+    public ModelAndView showMonopolyPage(HttpServletRequest request) {
+        //Obtengo la sesion
+        HttpSession session = request.getSession();
+        //Establezco como valor inicial la casilla 1
+        session.setAttribute("numeroRandom",1);
+        ModelMap mp  = new ModelMap();
+        mp.put("posicion", session.getAttribute("numeroRandom"));
+        return new ModelAndView("monopoly.html",mp);
+    }
+
+    @RequestMapping("/moverJugador")
+    public ModelAndView moverJugador(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        this.servicioMonopoly.obtenerPosicionCasillero(session);
+        ModelMap mp = new ModelMap();
+        mp.put("posicion", session.getAttribute("numeroRandom"));
+        return new ModelAndView("monopoly.html",mp);
+    }
 }
 
 
