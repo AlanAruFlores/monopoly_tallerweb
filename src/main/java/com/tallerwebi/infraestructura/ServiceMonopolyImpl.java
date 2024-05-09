29662dc8e8bce805dff90bcd744a8f475e22762e
AlanAruFlores <alanaruquipa242@gmail.com>
2024-05-09 07:28:35 -0300

    ventana monopoly y dado

--- src/main/java/com/tallerwebi/infraestructura/ServiceMonopolyImpl.java
+++ src/main/java/com/tallerwebi/infraestructura/ServiceMonopolyImpl.java
@@ -14,12 +14,10 @@ public class ServiceMonopolyImpl implements ServicioMonopoly {
     @Override
     public void obtenerPosicionCasillero(HttpSession session) {
         Integer numeroRandom = (int) (1 + (Math.random() * (6 - 1)));
-
+        session.setAttribute("dado",numeroRandom);
         Integer numeroSesion;
         numeroSesion = (Integer) session.getAttribute("numeroRandom");
-
         numeroRandom += numeroSesion;
-
         if(numeroRandom > 20){
             numeroRandom = (numeroRandom-20) ;
         }
